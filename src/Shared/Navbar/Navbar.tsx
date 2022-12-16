1e9d10000c36aea66ad977646729fe2927a9495c
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-12-17 01:01:25 +0600

    Project basic setup completed

---
+++ src/Shared/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from "../../images/logo.jpg"
+
+const Navbar = () => {
+    return (
+        <div className="px-20 bg-gray-200 flex items-center justify-between py-3">
+            <div className='flex items-center gap-4'>
+                <img className='w-14 h-14 rounded-full' src={logo} alt="" />
+                <h2 className='font-bold text-3xl'>HB Medical</h2>
+            </div>
+            <div>
+                <ul className='flex gap-10'>
+                <li>Home</li>
+                <li>Blogs</li>
+                <li>Login</li>
+            </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
