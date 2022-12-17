9e03d466acf29eaeddc64bc6d26ce8c6f87c71fd
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-12-17 15:27:29 +0600

    Service page done

--- src/Pages/Home/Home/Home.tsx
+++ src/Pages/Home/Home/Home.tsx
@@ -1,9 +1,11 @@
+import Services from "../../Services/Services";
 import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Services />
         </div>
     );
 };
---
+++ src/Pages/Services/Services.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import pathology from "../../images/pathology.png"
+import mri from "../../images/mri.png"
+import checkup from "../../images/checkup.png"
+import doctorLogo from "../../images/doctor-logo.png"
+import digitalXRay from "../../images/digital-x-ray.png"
+
+const Services = () => {
+    return (
+        <div className='grid grid-cols-5 gap-10 my-20 px-10 text-center'>
+            <div className='shadow-xl p-4 rounded-md'>
+                <img className='h-40 w-full' src={pathology} alt="" />
+                <h3 className="text-2xl">Pathology</h3>
+            </div>
+            <div className='shadow-xl p-4 rounded-md'>
+                <img className='h-40 w-full' src={mri} alt="" />
+                <h3 className="text-2xl">MRI</h3>
+            </div>
+            <div className='shadow-xl p-4 rounded-md'>
+                <img className='h-40 w-full' src={checkup} alt="" />
+                <h3 className="text-2xl">Madical Checkup</h3>
+            </div>
+            <div className='shadow-xl p-4 rounded-md'>
+                <img className='h-40 w-full' src={doctorLogo} alt="" />
+                <h3 className="text-2xl">Specialized Doctor</h3>
+            </div>
+            <div className='shadow-xl p-4 rounded-md'>
+                <img className='h-40 w-full' src={digitalXRay} alt="" />
+                <h3 className="text-2xl">Digital X-ray</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
