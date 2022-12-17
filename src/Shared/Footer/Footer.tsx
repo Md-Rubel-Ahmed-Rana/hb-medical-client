6417d6891e60574c985625cfd0801704eea6c777
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-12-17 15:52:21 +0600

    Footer completed

---
+++ src/Shared/Footer/Footer.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <footer className='flex justify-between px-40 py-10 bg-slate-300'>
+            <div>
+                <p><Link to="/" className="link link-hover">Branding</Link></p>
+                <p><Link to="/" className="link link-hover">Design</Link></p>
+                <p><Link to="/" className="link link-hover">Marketing</Link></p>
+                <p><Link to="/" className="link link-hover">Advertisement</Link></p>
+            </div> 
+            <div>
+               <p> <Link to="/" className="link link-hover">About us</Link></p>
+                <p><Link to="/" className="link link-hover">Contact</Link></p>
+                <p><Link to="/" className="link link-hover">Jobs</Link></p>
+                <p><Link to="/" className="link link-hover">Press kit</Link></p>
+            </div> 
+            <div>
+                <p><Link to="/" className="link link-hover">Terms of use</Link></p>
+                <p><Link to="/" className="link link-hover">Privacy policy</Link></p>
+                <p><Link to="/" className="link link-hover">Cookie policy</Link></p>
+            </div>
+</footer>
+    );
+};
+
+export default Footer;
