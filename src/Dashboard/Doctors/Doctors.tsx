18510ad3cf70bc3b7d1c9bf0c951ebad51e1b716
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-12-18 22:33:32 +0600

    Doctors and patients placeholder text added

--- src/Dashboard/Doctors/Doctors.tsx
+++ src/Dashboard/Doctors/Doctors.tsx
@@ -3,17 +3,39 @@ import React from 'react';
 const Doctors = () => {
     return (
         <div>
-            <h3 className='text-2xl'>All Doctors</h3>
-            <table>
-                <thead>
-                    <th></th>
+            <table className='w-full'>
+                <thead className='flex justify-between gap-10 bg-blue-700 p-2 rounded text-white'>
+                    <th>SR.</th>
                     <th>Avatar</th>
                     <th>Name</th>
                     <th>Email</th>
                     <th>Phone</th>
                     <th>Age</th>
                     <th>Designation</th>
+                    <th>Action</th>
                 </thead>
+                <tbody>
+                    <tr className='flex items-center justify-between gap-10  p-2 rounded border mt-2'>
+                        <td>1</td>
+                        <td>Image</td>
+                        <td>Rubel</td>
+                        <td>rubel@gmail.com</td>
+                        <td>01758049882</td>
+                        <td>22</td>
+                        <td>Pathology</td>
+                        <td><button className='bg-red-700 p-2 rounded text-white'>Delete</button></td>
+                    </tr>
+                    <tr className='flex items-center justify-between gap-10  p-2 rounded border mt-2'>
+                        <td>1</td>
+                        <td>Image</td>
+                        <td>Rubel</td>
+                        <td>rubel@gmail.com</td>
+                        <td>01758049882</td>
+                        <td>22</td>
+                        <td>Pathology</td>
+                        <td><button className='bg-red-700 p-2 rounded text-white'>Delete</button></td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     );
--- src/Dashboard/Patients/Patients.tsx
+++ src/Dashboard/Patients/Patients.tsx
@@ -3,7 +3,40 @@ import React from 'react';
 const Patients = () => {
     return (
         <div>
-            <h3 className="text-2xl">All Patients</h3>
+            <table className='w-full'>
+                <thead className='flex justify-between gap-10 bg-blue-700 p-2 rounded text-white'>
+                    <th>SR.</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Age</th>
+                    <th>Department</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    <tr className='flex items-center justify-between gap-10  p-2 rounded border mt-2'>
+                        <td>1</td>
+                        <td>Rubel</td>
+                        <td>rubel@gmail.com</td>
+                        <td>01758049882</td>
+                        <td>22</td>
+                        <td>Pathology</td>
+                        <td><button className='bg-green-500 p-2 rounded text-white'>Seen</button></td>
+                        <td><button className='bg-red-700 p-2 rounded text-white'>Delete</button></td>
+                    </tr>
+                    <tr className='flex items-center justify-between gap-10  p-2 rounded border mt-2'>
+                        <td>1</td>
+                        <td>Rubel</td>
+                        <td>rubel@gmail.com</td>
+                        <td>01758049882</td>
+                        <td>22</td>
+                        <td>Pathology</td>
+                        <td><button className='bg-green-500 p-2 rounded text-white'>Unseen</button></td>
+                        <td><button className='bg-red-700 p-2 rounded text-white'>Delete</button></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
